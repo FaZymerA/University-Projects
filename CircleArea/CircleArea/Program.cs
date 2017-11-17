daf87e092f90ba7c39c654a9e0b3792753c8b8f0
Dimitar Srebrinov <mitko.srebrinov@abv.bg>
2017-11-17 16:55:04 +0200

    These are tasks from control Ex1

---
+++ CircleArea/CircleArea/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            //височина 
+            int rand = random.Next(33, 188);
+            Console.WriteLine("The triagle random heigh is = " + rand);
+            double area = (rand * rand) * Math.PI;
+            double arr = Math.Round(area, 2);
+            Console.WriteLine(arr);
+
+        }
+    }
+}
